1aa9aca0e6c120eb8c0b60ab00cb4ecf3a6d0092
francofami <31368175+francofami@users.noreply.github.com>
2018-03-22 12:08:47 -0300

    Clase 1

--- Clase 1/main.c
+++ Clase 1/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 
 int main()
 {
@@ -11,10 +10,29 @@ int main()
     char: %c
     */
 
-    int numero1=88;
-    float numero2=33.2232;
+    int numero1;
+    float numero2;
+    float suma;
+    char letra;
 
-    printf("El numero entero es: %d \nEl numero flotante es: %.2f \n", numero1, numero2); //El .2 hace que me muestre 2 decimales despues de la coma
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &numero1);
+    printf("Ingrese un numero con coma: ");
+    scanf("%f", &numero2);
+    printf("Ingrese una letra: ");
+    letra = getche();
+
+    //letra=getch(); Cuando apretemos una tecla no se dibuja en pantalla
+    //letra=getche(); Cuando apretemos una tecla se va a dibujar en pantalla
+    //fflush(stdin); Opcion para limpiar el buffer de entrada para que podamos ingresar un caracter se pone antes de scanf("%c",&letra);
+    //scanf("% c", &letra); Otra opcion: dejo un espacio entre % y c para que me lea el caracter.
+
+    suma=numero1+numero2;
+
+    printf("\nEl numero entero es: %d \nEl numero flotante es: %.2f \n", numero1, numero2); //El .2 hace que me muestre 2 decimales despues de la coma
+    printf("La suma de los numeros es: %.2f\n",suma);
+    printf("La letra es: %c\n", letra);
+
 
 
     /*

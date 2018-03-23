514c1e001e172a1e1836cc0f2d2803c30c7b5e92
francofami <31368175+francofami@users.noreply.github.com>
2018-03-23 11:06:15 -0300

    Add files via upload

--- Clase 2/Ejercicio 1.c
+++ Clase 2/Ejercicio 1.c
@@ -13,9 +13,9 @@ int main()
         * Cantidad de números comprendidos entre 125 y 236.
     */
 
-    int numero, porcentajePositivos, porcentajeNegativos, maximo, minimo, maximopar=0;
+    float porcentajePositivos, porcentajeNegativos;
     char respuesta;
-    int contadorPares=0, contadorImpares=0, contadorPositivos=0, contadorNegativos=0, contador=0, contadorDos=0;
+    int contadorPares=0, contadorImpares=0, contadorPositivos=0, contadorNegativos=0, contador=0, contadorDos=0, numero, maximo, minimo, maximopar=0;
 
     do
     {
@@ -24,7 +24,7 @@ int main()
         printf("Ingrese numero (distinto de 0): ");
         scanf("%d", &numero);
 
-        while(numero==0 || numero)
+        while(numero==0)
         {
             printf("¡Error! Ingrese numero (distinto de 0): ");
             scanf("%d", &numero);
@@ -82,11 +82,14 @@ int main()
 
     printf("La cantidad de pares es: %d\n", contadorPares);
     printf("La cantidad de impares es: %d\n", contadorImpares);
-    printf("El porcentaje de numeros positivos es: %d\n", porcentajePositivos);
-    printf("El porcentaje de numeros negativos es: %d\n", porcentajeNegativos);
+    printf("El porcentaje de numeros positivos es: %.2f\n", porcentajePositivos);
+    printf("El porcentaje de numeros negativos es: %.2f\n", porcentajeNegativos);
     printf("El maximo es: %d\n", maximo);
     printf("El minimo es: %d\n", minimo);
+    if(maximopar!=0)
     printf("El maximo numero par es: %d\n", maximopar);
+    else
+        printf("No hay maximo numero par.\n");
     printf("La cantidad de numeros comprendidos entre 125 y 236 es: %d", contadorDos);
 
     return 0;

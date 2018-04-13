e51430cf15a6d6938b500c100790034ae9952991
francofami <31368175+francofami@users.noreply.github.com>
2018-04-13 10:26:10 -0300

    Add files via upload

--- Clase 6/Burbujeo/Burbujeo/main.c
+++ Clase 6/Burbujeo/Burbujeo/main.c
@@ -3,6 +3,26 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int i, j, aux;
+    int vector[5]={6,2,7,3,7};
+
+    for(i=0;i<4;i++)
+    {
+        for(j=i+1; j<5; j++)
+        {
+            if(vector[j]<vector[i]) //Menor a mayor
+            {
+                aux=vector[j];
+                vector[j]=vector[i];
+                vector[i]=aux;
+            }
+        }
+    }
+
+    for(i=0;i<5;i++)
+    {
+        printf("%d",vector[i]);
+    }
+
     return 0;
 }
